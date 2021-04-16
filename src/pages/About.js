6c689e23b440fb262f5efd17a4f22938007aaea7
jxhnkndl <jkroyston@gmail.com>
2021-04-16 17:33:05 -0400

    Set up temporary page components to test hash router component when deployed to github pages

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+// Import modules
+import React from 'react';
+import Alert from 'react-bootstrap/Alert';
+
+// Landing page component
+function About() {
+  return (
+    <Alert variant={'info'}>
+      About page alert
+    </Alert>
+  )
+}
+
+// Export component
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+// Import modules
+import React from 'react';
+import Alert from 'react-bootstrap/Alert';
+
+// Landing page component
+function Home() {
+  return (
+    <Alert variant={'success'}>
+      Landing page alert
+    </Alert>
+  )
+}
+
+// Export component
+export default Home;

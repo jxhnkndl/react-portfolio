99a07047931e0eb39aea39bfaf53b385351ca7fb
jxhnkndl <jkroyston@gmail.com>
2021-04-16 16:41:43 -0400

    Init core App.js component and index.js module

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// Import modules
+import React from 'react';
+
+// App component
+function App() {
+  return (
+    <h1>J.K. Royston</h1>
+  );
+}
+
+// Export component
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+// Import modules
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Render App component
+ReactDOM.render(<App />, document.getElementById('root'));

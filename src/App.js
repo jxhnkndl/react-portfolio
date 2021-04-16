a6fc0b4eb1603f10a33ebf36f5d241bb05b22b17
jxhnkndl <jkroyston@gmail.com>
2021-04-16 17:33:29 -0400

    Set up basic placeholder hash router to test on github pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 // Import modules and components
 import React from 'react';
+import { HashRouter, Route, Link } from 'react-router-dom';
 import Alert from 'react-bootstrap/Alert';
+import Home from './pages/Home';
+import About from './pages/About';
 
 // App component
 function App() {
   return (
-    <Alert variant={'danger'}>
-      Hi! I'm an alert!
-    </Alert>
+
+    <div>
+      <HashRouter basename="/">
+        <Alert variant={'danger'}>
+          Hi! I'm an alert!
+        </Alert>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+        <div className="container">
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+        </div>
+      </HashRouter>
+    </div>
   );
 }
 
 // Export component
-export default App;
+export default App; 

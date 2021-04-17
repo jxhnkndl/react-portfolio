6bc77c85bf5b938bfae6397cef05f98f2d93e9f1
jxhnkndl <jkroyston@gmail.com>
2021-04-17 19:47:44 -0400

    Set up HashRouter and test loading temp page components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 // Import modules and components
 import React from 'react';
-// import { HashRouter } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
+import MainNav from './components/MainNav';
+import Home from './components/Home';
+import About from './components/About';
 
 // App component
 function App() {
   return (
-    <div className="bg-primary">
-      <h1 className="text-light text-center">TEST</h1>
-    </div>
+    <HashRouter basename="/">
+      <MainNav />
+      <Route exact path={["/", "/home"]} component={Home} />
+      <Route exact path="/about" component={About} />
+    </HashRouter>
   );
 }
 

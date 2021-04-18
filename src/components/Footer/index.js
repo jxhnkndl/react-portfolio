2747e62bf5003cfdf0dd95c25b21c25744d0a0f2
jxhnkndl <jkroyston@gmail.com>
2021-04-18 17:31:12 -0400

    Re-style navbar and footer components

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,32 +7,38 @@ import './style.css';
 // Create and export footer component
 export default function Footer() {
   return (
-    <footer id="contact" class="container bg-green py-3">
-      <div class="row">
+    <footer className="py-2">
+      <div className="container">
+        <div className="row">
 
-        {/* Contact Details */}
-        <div class="col-6 text-right border-right border-white">
-          <a href="#navbar" class="d-block my-1 px-2 contact-link">J.K. ROYSTON</a>
-          <a href="tel:+18048337070" class="d-block my-1 px-2 contact-link">(804) 833-7070</a>
-          <a href="mailto:jkroyston@gmail.com" class="d-block my-1 px-2 contact-link">jkroyston@gmail.com</a>
-        </div>
+          <div className="col-1 col-md-2 col-lg-3"></div>
+          
+          {/* Contact Details */}
+          <div className="col-5 col-md-4 col-lg-3 text-right border-right border-white">
+            <a href="#navbar" className="my-1 px-2 contact-link">J.K. ROYSTON</a>
+            <a href="tel:+18048337070" className="my-1 px-2 contact-link">(804) 833-7070</a>
+            <a href="mailto:jkroyston@gmail.com" className="my-1 px-2 contact-link">jkroyston@gmail.com</a>
+          </div>
 
-        {/* Github + LinkedIn Profile Links */}
-        <div class="col-6">
-          <a class="contact-link my-1 px-2 d-block" href="https://github.com/jxhnkndl" target="_blank" rel="noreferrer">
-            <i class="fab fa-github mb-0 mr-1"></i>
-            GITHUB
-          </a>
-          <a class="contact-link my-1 px-2 d-block" href="https://www.linkedin.com/in/johnkendallroyston/" target="_blank" rel="noreferrer">
-            <i class="fab fa-linkedin mb-0 mr-1"></i>
-            LINKEDIN
-          </a>
-          <p class="d-block my-1 px-2 footer-text">
-            Copyright &copy; 2021
-          </p>
+          {/* Github + LinkedIn Profile Links */}
+          <div className="col-5 col-md-4 col-lg-3">
+            <a className="contact-link my-1 px-2" href="https://github.com/jxhnkndl" target="_blank" rel="noreferrer">
+              <i className="fab fa-github mb-0 mr-1"></i>
+              GITHUB
+            </a>
+            <a className="contact-link my-1 px-2" href="https://www.linkedin.com/in/johnkendallroyston/" target="_blank" rel="noreferrer">
+              <i className="fab fa-linkedin mb-0 mr-1"></i>
+              LINKEDIN
+            </a>
+            <p className="my-1 px-2 footer-text">
+              Copyright &copy; 2021
+            </p>
+          </div>
+
+          <div className="col-1 col-md-2 col-lg-3"></div>
+
         </div>
       </div>
-    
     </footer>
   );
 }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,21 +5,39 @@ import './style.css';
 // Create and export navbar component
 export default function Navbar() {
   return (
-    <nav style={{ height: '50px' }} id="navbar" className="navbar navbar-expand-lg">
-      <div className="container d-flex justify-content-center align-items-center py-2">
-        <div className="d-flex justify-content-end align-items-center">
+    <nav
+      style={{ height: '60px' }}
+      id="navbar"
+      className="navbar navbar-expand-lg"
+    >
+      <div>
+        {/* Primary Links */}
+        <a href="#header" className="nav-link px-2 rounded">
+          HOME
+        </a>
+        <a href="#portfolio" className="nav-link px-2 rounded">
+          PORTFOLIO
+        </a>
+        <a href="#about" className="nav-link px-2 rounded">
+          ABOUT
+        </a>
+        <a href="#contact" className="nav-link px-2 rounded">
+          CONTACT
+        </a>
 
-          {/* Primary Links */}
-          <a href="#header" className="nav-link px-2">HOME</a>
-          <a href="#portfolio" className="nav-link px-2">PORTFOLIO</a>
-          <a href="#about" className="nav-link px-2">ABOUT</a>
-          <a href="#contact" className="nav-link px-2">CONTACT</a>
-
-          {/* Secondary Links */}
-          <a href="https://www.github.com/jxhnkndl" className="d-none d-md-inline nav-link px-2">GITHUB</a>
-          <a href="https://www.linkedin.com/in/johnkendallroyston" className="d-none d-md-inline nav-link px-2">LINKEDIN</a>
-
-        </div>
+        {/* Secondary Links */}
+        <a
+          href="https://www.github.com/jxhnkndl"
+          className="d-none d-md-inline nav-link px-2 rounded"
+        >
+          GITHUB
+        </a>
+        <a
+          href="https://www.linkedin.com/in/johnkendallroyston"
+          className="d-none d-md-inline nav-link px-2 rounded"
+        >
+          LINKEDIN
+        </a>
       </div>
     </nav>
   );

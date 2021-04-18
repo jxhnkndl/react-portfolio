f41000aa6aba1806423d0db783a5e6bc68809bb2
jxhnkndl <jkroyston@gmail.com>
2021-04-18 18:18:55 -0400

    Add data from four projects to data object to use in rendering project cards

--- src/data.js
+++ src/data.js
@@ -1,10 +1,73 @@
 // Import screenshots
-import imgBurger from './assets/burger-js.png'
+import imgBurger from './assets/burger-js.png';
+import imgFooBar from './assets/foo-bar-finder.png';
+import imgJoglogger from './assets/joglogger.png';
+import imgWeather from './assets/weather-dashboard.png';
 
 // Export project data
 export const data = [
   {
     title: "Burger.js",
-    thumbnail: imgBurger
-  }
+    thumbnail: imgBurger,
+    stackStr: "UI / UX / Node.js / Express / MySQL / Handlebars.js / Bootstrap",
+    stackArr: [
+      "UI/UX",
+      "Node.js",
+      "Express",
+      "MySQL",
+      "Handlebars.js",
+      "JavaScript",
+      "Bootstrap"
+    ],
+    description: "The vanilla todo app just got a whole lot tastier. Burger.js is small full-stack application for keeping track of the burgers you want to eat and the burgers you've successfully eaten.",
+    app: "https://powerful-retreat-22028.herokuapp.com/",
+    repo: "https://github.com/jxhnkndl/burger"
+  },
+  {
+    title: "Foo Bar Finder",
+    thumbnail: imgFooBar,
+    stackStr: "UI / UX / Bootstrap / JavaScript / jQuery",
+    stackArr: [
+      "UI/UX",
+      "Bootstrap",
+      "JavaScript",
+      "jQuery"
+    ],
+    description: "Foo Bar Finder offers foodies a fresh way to find new recipes and cocktails to try. Users can search by main ingredient or category, cycle through a variety of results, and save favorites locally.",
+    app: "https://todtsies.github.io/Project-1/",
+    repo: "https://github.com/jxhnkndl/foo-bar-finder"
+  },
+  {
+    title: "Joglogger",
+    thumbnail: imgJoglogger,
+    stackStr: "UI / UX / Node.js / Express / MySQL / Handlebars.js / jQuery",
+    stackArr: [
+      "UI/UX",
+      "Node.js",
+      "Express",
+      "MySQL",
+      "Sequelize",
+      "Handlebars.js",
+      "JavaScript",
+      "jQuery"
+    ],
+    description: "Joglogger is a full-stack application for planning and completing workouts. Users can create, view, complete, and delete workouts from the database through a clean, minimalistic user interface.",
+    app: "https://vast-inlet-10059.herokuapp.com/add_workout",
+    repo: "https://github.com/jxhnkndl/workout-log"
+  },
+  {
+    title: "Weather Dashboard",
+    thumbnail: imgWeather,
+    stackStr: "UI / UX / Bootstrap / JavaScript / jQuery / Day.js",
+    stackArr: [
+      "UI/UX",
+      "Bootstrap",
+      "JavaScript",
+      "jQuery",
+      "Day.js"
+    ],
+    description: "Weather Dashboard fetches current weather and five-day forecast data for any city in the world using the OpenWeather API. Previously searched cities can be saved locally for future recall.",
+    app: "https://jxhnkndl.github.io/weather-dashboard",
+    repo: "https://github.com/jxhnkndl/weather-dashboard"
+  },
 ]

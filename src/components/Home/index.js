535680909c36c67c7a38ae1397abf4a724de8036
jxhnkndl <jkroyston@gmail.com>
2021-04-18 21:39:34 -0400

    Adjust positioning and styling of home page elements

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,23 +8,23 @@ import './style.css';
 // Landing page component
 function Home() {
   return (
-    <div id="header" class="container d-flex justify-content-center">
-      <div id="header-text" class="text-center">
+    <div id="header" className="container d-flex justify-content-center">
+      <div id="header-text" className="text-center">
 
-        <div id="img-wrapper" class="mx-auto mb-3 d-flex justify-content-center align-items-center">
+        <div id="img-wrapper" className="mx-auto mb-3 d-flex justify-content-center align-items-center">
           <img src={Animation} alt="Animated memoji portrait" />
         </div>
 
-        <h1 class="heading font-weight-bold">
-          <i id="header-icon" class="mb-0 mr-1 fas fa-code"></i>
+        <h1 className="heading font-weight-bold">
+          <i id="header-icon" className="mb-0 mr-1 fas fa-code"></i>
           HELLO!
         </h1>
 
-        <p class="h5 font-weight-light">My name is J.K. Royston and I'm a front-end web developer and designer from Richmond, Virgina.</p>
+        <p className="h5 font-weight-light">My name is J.K. Royston and I'm a front-end web developer and designer from Richmond, Virgina.</p>
 
         <hr />
 
-        <a href="./assets/docs/jk-royston-full-resume.pdf" class="btn green-gradient-bg light-color shadow">View My Portfolio</a>
+        <a href="./assets/docs/jk-royston-full-resume.pdf" className="btn cta shadow">View My Portfolio</a>
 
       </div>
     </div>

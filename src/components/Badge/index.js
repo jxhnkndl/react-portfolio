8373f8ba7c25c4b315ac4984dd3930959c3444cd
jxhnkndl <jkroyston@gmail.com>
2021-04-19 16:47:57 -0400

    Invert badge color scheme

--- src/components/Badge/index.js
+++ src/components/Badge/index.js
@@ -5,7 +5,7 @@ import './style.css';
 // Create and export badge component
 export default function Badge(props) {
   return (
-    <div className="skill-badge rounded mb-2 mr-2 p-2">
+    <div className="skill-badge rounded mb-2 mr-2 p-2 shadow">
       <i className={`skill-icon ${props.icon} mb-2`}></i>
       <p className="d-block mb-0">{props.tech}</p>
     </div>

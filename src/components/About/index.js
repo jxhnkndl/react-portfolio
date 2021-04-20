16aa6f478e751ba14fef2d8951cb3f652a56c416
jxhnkndl <jkroyston@gmail.com>
2021-04-20 19:22:47 -0400

    Adjust general element shadow sizes and opacity

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -15,7 +15,7 @@ export default function About() {
           <div className="col-12 col-md-3 mb-4">
             <img className="img-fluid rounded mb-4 shadow" src={headshot} alt="Professional headshot" />
 
-            <div className="contact-details d-none d-lg-block p-2 rounded shadow">
+            <div className="contact-details d-none d-lg-block p-2 rounded">
               <h6 className="contact-heading">CONTACT DETAILS</h6>
               <p className="bio-text font-weight-light mb-1">J.K. Royston</p>
               <p className="bio-text font-weight-light mb-1">(804) 833-7070</p>
